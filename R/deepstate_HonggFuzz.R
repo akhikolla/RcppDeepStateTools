25fd2c146537dc270173b1ef4930b60bcd44444d
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-09-29 22:12:25 -0700

    honggfuzz dir

--- R/deepstate_HonggFuzz.R
+++ R/deepstate_HonggFuzz.R
@@ -3,11 +3,12 @@
 deepstate_make__hongg <- function(){
   insts.path <- "~"
   deepstate.path <- paste0(insts.path,"/.RcppDeepState")
-  system(paste0("cd ", deepstate.path, " ; ","git clone https://github.com/google/honggfuzz && cd honggfuzz && make"))
   master <- file.path(deepstate.path,"deepstate-master")
   build_honggfuzz <- file.path(master,"build_honggfuzz")
   dir.create(build_honggfuzz,showWarnings = FALSE)
-  system("export HONGGFUZZ_HOME=\"/honggfuzz\"")
+  system(paste0("cd ", build_honggfuzz, " ; ","git clone https://github.com/google/honggfuzz && cd honggfuzz && make"))
+  hongg.path <- file.path(build_honggfuzz,"honggfuzz")
+  system(paste0("export HONGGFUZZ_HOME=","\"",hongg.path,"\""))
   system(paste0("cd ", build_honggfuzz," ; ","CXX=\"$HONGGFUZZ_HOME/hfuzz_cc/hfuzz-clang++\" CC=\"$HONGGFUZZ_HOME/hfuzz_cc/hfuzz-clang\" cmake -DDEEPSTATE_HONGGFUZZ=ON ../"," ; ", "make"))
 
 }
