d40ea492d5c3ca4e5acc73aed1254e8cc5d7ab63
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-11-24 16:11:36 -0700

    valgrind afl code added

--- R/valgrind_AFL.R
+++ R/valgrind_AFL.R
@@ -15,3 +15,35 @@ run_AFL<-function(){
   }
   }
 }
+
+
+valgrind_afl_test <- function(){
+  cA.dir <- file.path(system.file("extdata",package="RcppDeepState"),"compileAttributes")
+  cA.dir <-normalizePath(cA.dir, mustWork=TRUE)
+  root.path <- system.file("extdata",package="RcppDeepState")
+  packages <- Sys.glob(file.path(cA.dir,"*"))
+  for(pkg.i in seq_along(packages)){
+    pkg.tar.gz <- packages[[pkg.i]]
+    testfiles.vec <- Sys.glob(file.path(pkg.tar.gz,"inst/testfiles/*"))
+    test_path <- file.path(pkg.tar.gz,"inst/testfiles")
+    functions.list <-  RcppDeepState::deepstate_get_function_body(pkg.tar.gz)
+    cat(sprintf("%4d - %s\n", pkg.i, pkg.tar.gz))
+    if(length(testfiles.vec) > 0){
+      for(fun in testfiles.vec){
+        afl.fun.path <- file.path(test_path,basename(fun),paste0("AFL_",basename(fun)))
+        afl.inputs <- Sys.glob(file.path(afl.fun.path,"afl_inputs/*"))
+        afl.inputs.path <- file.path(afl.fun.path,"afl_inputs")
+        times <- unique(gsub("_.*","",basename(afl.inputs)))
+        for(utime in times){
+          run_line <- paste0("R -d \"valgrind --xml=yes --xml-file=",file.path(afl.inputs.path,paste0(utime,"_valgrind_log"))," --tool=memcheck --leak-check=yes\" --vanilla --args ",afl.inputs.path,"/",utime," < /home/akhila/RcppDeepStateTest/afl_valgrind_per_function.R")
+          print(run_line)
+          system(run_line)
+          print("valgrind result")
+          result = RcppDeepState::deepstate_read_valgrind_xml(file.path(afl.inputs.path,paste0(utime,"_valgrind_log")))
+          print(result$logtable)
+        }
+      }
+    }
+  }
+}
+
